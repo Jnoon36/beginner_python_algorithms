265e2c7d2ae89512c06626c7a97536fba0d8bc2a
Jacob <Jacob.noon@rockets.utoledo.edu>
2019-01-22 20:26:18 -0600

    Update and rename numerical_methods/bisection_method/bisection.py to numerical_methods/bisection.py

--- numerical_methods/bisection.py
+++ numerical_methods/bisection.py
@@ -1,3 +1,14 @@
+"""
+When we want to find the zero (root) of a function f(x) in the plane, it is not always possible to set f equal to zero 
+and analytically solve for x. Just try f(x) = cos(x) - x. Therefore, we need what's called a root-finding algorithm. The 
+bisection method is one of the most common ways to find a zero of a function. If a function f(x) is continuous on a closed
+interval [a, b], and f(a)*f(b)<0 (f has opposite signs at the endpoints), then there exists a c in [a, b] such that f(c) = 0.
+More can be found on this method and further generalizations of this method at https://en.wikipedia.org/wiki/Bisection_method
+
+
+Feel free to use this method and play around with it!!!
+"""
+
 def bisection(f, a, b, tol, N):
     """
     N is the maximum number of iterations, tol is the tolerance (how accurate of a solution you want).
